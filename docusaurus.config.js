740cf062c9f5482da53b54357a260cc75bfa28d0
Hope Hadfield <hhadfiel@redhat.com>
2024-03-14 15:03:53 -0400

    Set trailingSlash to false

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,6 +22,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'redhat-developer', // Usually your GitHub org/user name.
   projectName: 'idetools.dev', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
