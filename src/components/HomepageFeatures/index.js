0f772856d7abbfd7001e797cbba777704edc3f58
Hope Hadfield <hhadfiel@redhat.com>
2024-04-20 13:35:03 -0400

    Add LSP4IJ to landing page

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -10,35 +10,39 @@ export default function HomepageFeatures() {
         <p></p><p></p>
           <h1 styles="padding-top:1em">Language Extensions</h1>
           <span>
-          <div className={styles.imgCol}>
-            <a href="https://marketplace.visualstudio.com/items?itemName=redhat.java"><img src='img/vscode-java.png' height='128em'></img></a>
-            <figcaption position='absolute'><b><a href="https://github.com/redhat-developer/vscode-java">VS Code Java</a></b></figcaption>
-          </div>
-          <div className={styles.imgCol}>
-            <a href="https://marketplace.visualstudio.com/items?itemName=redhat.vscode-yaml"><img src='img/yaml.png' height='128em'></img></a>
-            <figcaption position='absolute'><b><a href="https://github.com/redhat-developer/vscode-yaml">YAML</a></b></figcaption>
-          </div>
-          </span>
-          <span>
-          <div className={styles.imgCol}>
-            <a href="https://marketplace.visualstudio.com/items?itemName=redhat.vscode-xml"><img src='img/xml.png' height='128em'></img></a>
-            <figcaption position='absolute'><b><a href="https://github.com/redhat-developer/vscode-xml">XML</a></b></figcaption>
-          </div>
-          <div className={styles.imgCol}>
-          <a href="https://marketplace.visualstudio.com/items?itemName=redhat.vscode-quarkus"><img src='img/vscode-quarkus.png' height='128em'></img></a>
-            <figcaption position='absolute'><b><a href="https://github.com/redhat-developer/vscode-quarkus">VS Code Quarkus</a></b></figcaption>
-          </div>
-          </span>
-          <span>
-          <div className={styles.imgCol}>
-          <a href="https://marketplace.visualstudio.com/items?itemName=redhat.vscode-microprofile"><img src='img/vscode-quarkus.png' height='128em'></img></a>
-            <figcaption position='absolute'><b><a href="https://github.com/redhat-developer/vscode-microprofile">Microprofile</a></b></figcaption>
-          </div>
-          <div className={styles.imgCol}>
-            <a href="https://plugins.jetbrains.com/plugin/13234-quarkus-tools"><img src='img/intellij-quarkus.png' height='128em'></img></a>
-            <figcaption position='absolute'><b><a href="https://github.com/redhat-developer/intellij-quarkus">IntelliJ Quarkus</a></b></figcaption>
-          </div>
-          </span>
+        <div className={styles.imgCol}>
+          <a href="https://marketplace.visualstudio.com/items?itemName=redhat.java"><img src='img/vscode-java.png' height='128em'></img></a>
+          <figcaption position='absolute'><b><a href="https://github.com/redhat-developer/vscode-java">VS Code Java</a></b></figcaption>
+        </div>
+        <div className={styles.imgCol}>
+          <a href="https://marketplace.visualstudio.com/items?itemName=redhat.vscode-yaml"><img src='img/yaml.png' height='128em'></img></a>
+          <figcaption position='absolute'><b><a href="https://github.com/redhat-developer/vscode-yaml">YAML</a></b></figcaption>
+        </div>
+        </span>
+        <span>
+        <div className={styles.imgCol}>
+          <a href="https://marketplace.visualstudio.com/items?itemName=redhat.vscode-xml"><img src='img/xml.png' height='128em'></img></a>
+          <figcaption position='absolute'><b><a href="https://github.com/redhat-developer/vscode-xml">XML</a></b></figcaption>
+        </div>
+        <div className={styles.imgCol}>
+        <a href="https://marketplace.visualstudio.com/items?itemName=redhat.vscode-quarkus"><img src='img/vscode-quarkus.png' height='128em'></img></a>
+          <figcaption position='absolute'><b><a href="https://github.com/redhat-developer/vscode-quarkus">VS Code Quarkus</a></b></figcaption>
+        </div>
+        </span>
+        <span>
+        <div className={styles.imgColThree}>
+        <a href="https://marketplace.visualstudio.com/items?itemName=redhat.vscode-microprofile"><img src='img/vscode-quarkus.png' height='128em'></img></a>
+          <figcaption position='absolute'><b><a href="https://github.com/redhat-developer/vscode-microprofile">Microprofile</a></b></figcaption>
+        </div>
+        <div className={styles.imgColThree}>
+          <a href="https://plugins.jetbrains.com/plugin/13234-quarkus-tools"><img src='img/intellij-quarkus.png' height='128em'></img></a>
+          <figcaption position='absolute'><b><a href="https://github.com/redhat-developer/intellij-quarkus">Intellij Quarkus</a></b></figcaption>
+        </div>
+        <div className={styles.imgColThree}>
+          <a href="https://plugins.jetbrains.com/plugin/23257-lsp4ij"><img src='img/intellij.png' height='128em'></img></a>
+          <figcaption position='absolute'><b><a href="https://github.com/redhat-developer/lsp4ij">LSP4IJ</a></b></figcaption>
+        </div>
+        </span>
         </div>
         <div className={styles.rightColumn}>
         <p></p><p></p>
