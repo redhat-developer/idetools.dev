4d61e4f2a655d23e10823ea90bb9460f87551c43
Hope Hadfield <hhadfiel@redhat.com>
2024-04-24 15:59:24 -0400

    Fix code formatting

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,38 +8,38 @@ import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <span>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="https://marketplace.visualstudio.com/publishers/redhat">
-            <div>
-              <img src="/img/visual-studio-code-icon.webp" alt="VS Code Logo"></img>
-              VS Code Marketplace
-            </div>
+        <div className="container">
+          <Heading as="h1" className="hero__title">
+            {siteConfig.title}
+          </Heading>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className="button button--secondary button--lg"
+              to="https://marketplace.visualstudio.com/publishers/redhat">
+              <div>
+                <img src="/img/visual-studio-code-icon.webp" alt="VS Code Logo"></img>
+                VS Code Marketplace
+              </div>
 
-          </Link>
-        </div>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="https://plugins.jetbrains.com/organizations/Red-Hat">
-            <div>
-            <img src="/img/jetbrains-logo.png" alt="JetBrains Logo"></img>
-            Jetbrains Marketplace
-            </div>
+            </Link>
+          </div>
+          <div className={styles.buttons}>
+            <Link
+              className="button button--secondary button--lg"
+              to="https://plugins.jetbrains.com/organizations/Red-Hat">
+              <div>
+                <img src="/img/jetbrains-logo.png" alt="JetBrains Logo"></img>
+                Jetbrains Marketplace
+              </div>
 
-          </Link>
+            </Link>
+          </div>
         </div>
-      </div>
       </span>
       <div>
         <img src="/img/RHD-logo-old.png" alt="Red Hat Developer Logo"></img>
@@ -49,7 +49,7 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={siteConfig.title}
